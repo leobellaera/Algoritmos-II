b9f6140b34c014329e37e7c8b3f522ff42fa7f7f
leobellaera <leobellaera@gmail.com>
2017-10-09 15:40:24 -0300

    Update lista_abb_mayores.c

--- lista_abb_mayores.c
+++ lista_abb_mayores.c
@@ -13,6 +13,9 @@ bool aux_lista_abb_mayores(const ab_t* arbol, lista_t* lista){
                 return NULL
         aux_lista_abb_mayores(arbol->izq);
         if (. .. . .)
-                lista_insertar_ultimo(lista, arbol->clave);
+                bool ok = lista_insertar_ultimo(lista, arbol->clave);
+                if (!ok)
+                        return false;
         aux_lista_abb_mayores(arbol->der);
+        return true;
        
