79826cfea9bd5c7d5cf29d1ba1b6ed5dcf221fb6
leobellaera <leobellaera@gmail.com>
2017-10-09 15:57:14 -0300

    Create lista_ordenados_arbol_orig.c

---
+++ lista_ordenados_arbol_orig.c
@@ -0,0 +1,4 @@
+/*Implementar una primitiva para el ABB, que reciba el ABB y devuelva una lista con las claves del mismo
+ordenadas tal que si insertáramos las claves en un ABB vacío (con la misma función de comparación),
+dicho ABB tendría la misma estructura que el árbol original. ¿Qué tipo de recorrido utilizaste?
+Indicar el orden de la primitiva. Justificar. */
