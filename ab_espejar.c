aff43f2ea1f6776a64112ea798c41d73534ccab6
leobellaera <leobellaera@gmail.com>
2017-10-09 15:05:15 -0300

    Update ab_espejar.c

--- ab_espejar.c
+++ ab_espejar.c
@@ -4,8 +4,8 @@ ab_t* ab_espejar(const abb_t* arbol){
         ab_t* arbol_espejado = crear_arbol(arbol->raiz);
         if (!arbol_espejado)
                 return NULL;
-        arbol_espejado->der = arbol_espejar(ab->izq);
-        arbol_espejado->izq = arbol_espejar(ab->der);
+        arbol_espejado->der = arbol_espejar(arbol->izq);
+        arbol_espejado->izq = arbol_espejar(arbol->der);
         return arbol_espejado;
 
         
